b42ce49bbe320c17ccec7ebbf14c28ad0ae37f72
Masataka Okada <masataka.okada1117@gmail.com>
2022-08-14 08:22:14 +0900

    typeormを設定

---
+++ src/config/ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  autoLoadEntities: true,
+  entities: ['dist/**/entities/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+  },
+} as DataSourceOptions);
