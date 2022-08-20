bb4f48363fe2e544d21da66d3455b394ba88ca2c
Masataka Okada <masataka.okada1117@gmail.com>
2022-08-20 10:17:48 +0900

    authモジュールの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import ormconfig from 'src/config/ormconfig';
 
 @Module({
-  imports: [ItemsModule, TypeOrmModule.forRoot(ormconfig)],
+  imports: [ItemsModule, TypeOrmModule.forRoot(ormconfig), AuthModule],
   controllers: [],
   providers: [],
 })
