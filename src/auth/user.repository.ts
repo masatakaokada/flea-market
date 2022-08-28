7ab4e3b455945561373f5280770f6516f716b419
Masataka Okada <masataka.okada1117@gmail.com>
2022-08-28 17:55:19 +0900

    パスワードのハッシュ化

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,16 @@ import { User } from 'src/entities/user.entity';
 import { CustomRepository } from 'src/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt'
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { username, password, status } = createUserDto;
-    const user = this.create({ username, password, status });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashPassword, status });
 
     await this.save(user);
     return user;
