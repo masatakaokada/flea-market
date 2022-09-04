1daf28db390b4a5f4b14b57c8cbd33cdf7a74094
Masataka Okada <masataka.okada1117@gmail.com>
2022-09-04 15:16:40 +0900

    guardの追加

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { TypeOrmExModule } from '../typeorm-ex.module';
 import { ItemRepository } from './item.repository';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([ItemRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([ItemRepository]), AuthModule],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
